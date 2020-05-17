b15c14a506cd25dc553ef0e72cdd7b34317bf0d8
proxy707 <princegaur1998@gmail.com>
2020-05-17 10:59:37 +0530

    file changed

--- learnflask2/application.py
+++ learnflask2/application.py
@@ -1,17 +1,17 @@
 import flask
-from flask import Flask,render_template
-app=Flask(__name__)
+from flask import Flask, render_template
+app= Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/about")
+@app.route("/about.html")
 def about():
     return render_template('about.html')
- 
-@app.route("/contact")
+
+@app.route("/contact.html")
 def contact():
     return render_template('contact.html')
-
+    
 app.run(debug=True)
