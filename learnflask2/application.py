355f629de24891974d60c345e5f0cd2e51719aac
proxy707 <princegaur1998@gmail.com>
2020-05-19 23:41:22 +0530

    file changed

--- learnflask2/application.py
+++ learnflask2/application.py
@@ -5,6 +5,13 @@ app= Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/db_name'
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+
+    
 @app.route("/")
 def home():
     return render_template('index.html')
