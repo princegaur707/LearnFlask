f5f23847061b06c493ac8b0eb39323c0a29055e4
proxy707 <princegaur1998@gmail.com>
2020-05-19 23:18:01 +0530

    file changed

--- learnflask2/application.py
+++ learnflask2/application.py
@@ -1,6 +1,8 @@
 import flask
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 app= Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db' 
 
 @app.route("/")
 def home():
