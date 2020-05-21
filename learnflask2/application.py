c8a0337a95ff7ad03e7f5d5774f71b91d5391fcb
proxy707 <princegaur1998@gmail.com>
2020-05-22 02:01:42 +0530

    file changed

--- learnflask2/application.py
+++ learnflask2/application.py
@@ -1,11 +1,12 @@
 import flask
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 app= Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/blogmaniac'
 db = SQLAlchemy(app)
 
-class contacts(db.Model):
+class Contacts(db.Model):
     
     SNo= db.Column(db.Integer, primary_key=True)
     Name = db.Column(db.String(80), unique=True,nullable=False)
@@ -19,7 +20,7 @@ class posts(db.Model):
     SNo= db.Column(db.Integer, primary_key=True)
     Title= db.Column(db.String(80), unique=True,nullable=False)
     Content= db.Column(db.String(120), unique=True,nullable=False)
-    Date= db.Column(db.String(120), unique=True)
+    Date= db.Column(db.String(120), nullable=True)
 
 @app.route("/")
 def home():
@@ -30,8 +31,18 @@ def about():
     return render_template('about.html')
 
 
-@app.route("/contact.html")
+@app.route("/contact.html",methods={'GET','POST'})
 def contact():
+    if  request.method=='POST':
+        #Add entry to database
+        name=request.form.get('name')
+        email=request.form.get('email')
+        phone=request.form.get('phone')
+        message=request.form.get('message')
+        entry=Contacts(Name=name, Email=email,Phone_number=phone,Message=message)
+        db.session.add(entry)
+        db.session.commit()
+        
     return render_template('contact.html')
 
 @app.route("/post.html")
